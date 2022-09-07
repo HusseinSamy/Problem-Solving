3f2001146af9e68ef5aaea9448a732e47e3facaf
Hussein Samy <hussein.samy02@gmail.com>
2022-09-07 20:36:27 +0200

    Adds simple calculator solution

---
+++ ICPC training level 0 sheet/level0.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <string> 
+#include <iostream>
+
+using namespace std;
+    
+void simple_calculator() {
+    long long first_number; 
+    long long second_number; 
+    cin >> first_number >> second_number;
+    
+    cout << first_number << " + " << second_number << " = "
+    << first_number + second_number << endl
+    << first_number << " * " << second_number << " = " 
+    << first_number * second_number << endl
+    << first_number << " - " << second_number << " = "
+    << first_number - second_number;
+}
+
+int main () {
+    
+}
+
