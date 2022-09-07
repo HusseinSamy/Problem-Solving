10d3876b4c5b65e181ebefa69995b0e4fc23bbee
Hussein Samy <hussein.samy02@gmail.com>
2022-09-07 20:37:42 +0200

    Adds basic data types solution

--- ICPC training level 0 sheet/level0.cpp
+++ ICPC training level 0 sheet/level0.cpp
@@ -24,6 +24,21 @@ void say_hello_in_cpp(){
     cout << "Hello, "<< name;
 }
 
+void basic_data_types(){
+    int a; 
+    long long b; 
+    char c; 
+    float d; 
+    double e; 
+    
+    cin >> a >> b >> c >> d >> e; 
+    cout << a << endl
+    << b << endl
+    << c << endl
+    << d << endl 
+    << e; 
+}
+
 int main () {
     
 }
